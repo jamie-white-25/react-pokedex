e4f8f49293392d0dfa48019e780d0c8432900f76
jamie-white-25 <cottagecabinetsltd@gmail.com>
2020-05-15 00:08:37 +0100

    Updates

---
+++ precache-manifest.47ddf52144015f195e5606b044349675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d4c82314cd637c0eb4f5b71772a55e",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "63f8465f95c599978c82",
+    "url": "/react-pokedex/static/css/main.20c18ad5.chunk.css"
+  },
+  {
+    "revision": "840cc01fd889c8a211b6",
+    "url": "/react-pokedex/static/js/2.5f6600e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokedex/static/js/2.5f6600e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f8465f95c599978c82",
+    "url": "/react-pokedex/static/js/main.80695a1c.chunk.js"
+  },
+  {
+    "revision": "5b793d67441adf0043f2",
+    "url": "/react-pokedex/static/js/runtime-main.ba09b3dd.js"
+  }
+]);
